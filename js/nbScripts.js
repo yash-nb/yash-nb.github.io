7438bdd1b89940df89dfc58789dbaae9cf7c932f
Yash Paliwal <yash@northernblock.ca>
2018-11-12 22:56:00 -0500

    Stable v1

--- js/nbScripts.js
+++ js/nbScripts.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
-  console.log('test')
   var slideIndex = 1
   var emailFormMsg = ''
+  var sliderLive = true
   showDivs(slideIndex)
 
   function plusDivsBack() {
-    console.log('plusDivs Back')
     showDivs((slideIndex -= 1))
   }
-  $('#slider-btn-left').click(plusDivsBack)
+  $('#slider-btn-left').click(() => {
+    sliderLive = false
+    plusDivsBack()
+  })
 
   function plusDivsForward() {
-    console.log('plusDivs Forward')
     showDivs((slideIndex += 1))
   }
-  $('#slider-btn-right').click(plusDivsForward)
+  $('#slider-btn-right').click(() => {
+    sliderLive = false
+    plusDivsForward()
+  })
 
   function showDivs(n) {
     var i
@@ -31,20 +35,71 @@ $(document).ready(function() {
     x[slideIndex - 1].style.display = 'block'
   }
 
-  setInterval(plusDivsForward, 5000)
+  setInterval(() => {
+    if (sliderLive) {
+      plusDivsForward()
+    }
+  }, 5000)
 
   // Email submission form
   function submitEmailAddress() {
-    let email = $('#email-input').val()
-    if (email === '') {
-      $('#emailInputMsg').text('Invalid email address.')
-      $('#emailInputMsg').show()
-    } else {
-      $('#emailInputMsg').text(
-        "Great, we'll be sending you email updates shortly."
+    if ($('#email-input').val() !== '') {
+      // Collect the data into an obj
+      let newMessage = {}
+      newMessage.email = $('#email-input').val()
+      newMessage.phone = $('#phone-input').val()
+      newMessage.message = $('#message-input').val()
+
+      console.log(newMessage)
+
+      // Make the jquery ajax call i guess
+      // $.ajax({
+      //   url:
+      //     'https://us-central1-norblock-c98d8.cloudfunctions.net/contactForm_cannatrac',
+      //   body: newMessage,
+      //   type: 'POST',
+      //   crossDomain: true,
+      //   dataType: 'json',
+      //   success: function(res) {
+      //     console.log(res)
+      //   },
+      //   error: function() {
+      //     console.log('Failed!')
+      //   }
+      //   // beforeSend: setHeader
+      // })
+      $.post(
+        'https://us-central1-norblock-c98d8.cloudfunctions.net/contactForm_cannatrac',
+        newMessage,
+        function(response) {
+          console.log(response)
+          if (response === 'Success.') {
+            showSuccessFailureMsg(true)
+          }
+        }
       )
-      $('#emailInputMsg').show()
+    } else {
+      showSuccessFailureMsg(false)
     }
   }
   $('#submit-email').click(submitEmailAddress)
+
+  // Post helpers
+  function showSuccessFailureMsg(bool) {
+    if (bool) {
+      $('#emailInputMsg').text("Great, we'll be in touch shortly.")
+      $('#emailInputMsg').show()
+      cleanUpForm()
+    } else {
+      $('#emailInputMsg').text('Invalid email address.')
+      $('#emailInputMsg').show()
+      cleanUpForm()
+    }
+  }
+
+  function cleanUpForm() {
+    $('#email-input').val('')
+    $('#phone-input').val('')
+    $('#message-input').val('')
+  }
 })
