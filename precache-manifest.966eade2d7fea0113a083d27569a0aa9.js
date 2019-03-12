30786245ef3d6986da26935c69aedf2534754b51
Yash Paliwal <yash@northernblock.ca>
2019-03-12 13:03:24 -0400

    new

---
+++ precache-manifest.966eade2d7fea0113a083d27569a0aa9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc327eaefb728fbe5fa1f151c8d7f30",
+    "url": "/static/media/contactForm-bg-logo.ccc327ea.png"
+  },
+  {
+    "revision": "b88822798c399868d303",
+    "url": "/static/css/main.11222eaf.chunk.css"
+  },
+  {
+    "revision": "e442165fe57d30520c20",
+    "url": "/static/js/1.e442165f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9612e314f3fd09528ffe738e9970145",
+    "url": "/static/media/logo.c9612e31.png"
+  },
+  {
+    "revision": "34e7f9fbf68dc6a17270d5dc834eda30",
+    "url": "/static/media/demoImg.34e7f9fb.png"
+  },
+  {
+    "revision": "633393457389f1e2d44dcd14c87cb416",
+    "url": "/static/media/section2-left.63339345.png"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/static/media/Futura Book font.f9f02ed0.ttf"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/static/media/futura light bt.98d2f973.ttf"
+  },
+  {
+    "revision": "3ea9695ee3af8a5e17413f22249f361a",
+    "url": "/static/media/Futura Heavy font.3ea9695e.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "9a1626276b430d216809407c3fbcda75",
+    "url": "/static/media/futur.9a162627.ttf"
+  },
+  {
+    "revision": "4a3d46ce44b11a8781b84bb139c1f852",
+    "url": "/static/media/Futura-Bold.4a3d46ce.woff"
+  },
+  {
+    "revision": "a21afac968d4c873c5ed7fc86006c2a8",
+    "url": "/static/media/Futura-Bold.a21afac9.woff2"
+  },
+  {
+    "revision": "b88822798c399868d303",
+    "url": "/static/js/main.b8882279.chunk.js"
+  },
+  {
+    "revision": "36c249f941086c355d58895812c49f5f",
+    "url": "/static/media/jumbo.36c249f9.png"
+  },
+  {
+    "revision": "67a9a43b5148a909370548a79be9bac6",
+    "url": "/static/media/white-logo.67a9a43b.png"
+  },
+  {
+    "revision": "1c1cff42a6db31c827c3c2123b673e1b",
+    "url": "/static/media/market-opp.1c1cff42.png"
+  },
+  {
+    "revision": "d1e7be1e37510f941ff5843f32ef5430",
+    "url": "/static/media/forCultivators.d1e7be1e.png"
+  },
+  {
+    "revision": "4eacf81ab921c033e27026d79405c5a0",
+    "url": "/static/media/forRetailers.4eacf81a.png"
+  },
+  {
+    "revision": "5865e82ad43818ebe10863cf282725e0",
+    "url": "/static/media/forRegulators.5865e82a.png"
+  },
+  {
+    "revision": "47815cc1777ebee79be9e838c5f10f5f",
+    "url": "/static/media/forPharmaClients.47815cc1.png"
+  },
+  {
+    "revision": "2cf8600d42cc49fa39f7eb07382872f9",
+    "url": "/static/media/whyBlockchain.2cf8600d.png"
+  },
+  {
+    "revision": "c6ca859ced7d78fe16bef03702f3dfe8",
+    "url": "/static/media/logo-nb.c6ca859c.png"
+  },
+  {
+    "revision": "a9442d01dad27296804ae3a2ca6710eb",
+    "url": "/static/media/logo-ap.a9442d01.png"
+  },
+  {
+    "revision": "a5c8c2a68b749710cf6f5f37d819063c",
+    "url": "/static/media/mathieu.a5c8c2a6.png"
+  },
+  {
+    "revision": "b204becedf0bbb533b116b64c5c42d9e",
+    "url": "/static/media/duncan.b204bece.png"
+  },
+  {
+    "revision": "435f26fedfc62ab02ecd8934ad1bd7bf",
+    "url": "/index.html"
+  }
+];
